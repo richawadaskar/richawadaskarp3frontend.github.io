4b9bef639675e3f0e1e6852cdb225f8442fd7c27
Richa Wadaskar <richa.wadaskar@gmail.com>
2019-10-30 08:32:34 -0700

    initial commit

---
+++ precache-manifest.322e039fd16d6e92fff6404646d7111c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b8a8789ccbbca36d9165c31bd18d8bcf",
+    "url": "/index.html"
+  },
+  {
+    "revision": "044d011443f7afb306bb",
+    "url": "/static/css/main.e970ffc5.chunk.css"
+  },
+  {
+    "revision": "680d799baefafd0b070f",
+    "url": "/static/js/2.cef5eb4b.chunk.js"
+  },
+  {
+    "revision": "044d011443f7afb306bb",
+    "url": "/static/js/main.1723937f.chunk.js"
+  },
+  {
+    "revision": "cd2485c73a32870978d1",
+    "url": "/static/js/runtime-main.5f18b2d7.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/static/media/logo.25bf045c.svg"
+  }
+]);
